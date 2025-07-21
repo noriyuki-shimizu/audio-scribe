fc454ee47cc29c6818ae71f90c9f0dd8ba9b5d70
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2025-07-21 23:22:14 +0900

    favicon の設定を変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,51 @@ export default defineNuxtConfig({
     }
   },
   modules: ['nuxt-svgo', '@nuxt/eslint'],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja'
+      },
+      link: [
+        {
+          rel: 'icon',
+          sizes: '24x24',
+          type: 'image/webp',
+          href: '/favicon/icon-24x24.webp'
+        },
+        {
+          rel: 'icon',
+          sizes: '32x32',
+          type: 'image/webp',
+          href: '/favicon/icon-32x32.webp'
+        },
+        {
+          rel: 'icon',
+          sizes: '36x36',
+          type: 'image/webp',
+          href: '/favicon/icon-36x36.webp'
+        },
+        {
+          rel: 'icon',
+          sizes: '48x48',
+          type: 'image/webp',
+          href: '/favicon/icon-48x48.webp'
+        },
+        {
+          rel: 'icon',
+          sizes: '192x192',
+          type: 'image/webp',
+          href: '/favicon/android-chrome-192x192.webp'
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          type: 'image/webp',
+          href: '/favicon/apple-touch-icon-180x180.webp'
+        }
+      ]
+    }
+  },
   imports: {
     dirs: [
       '@/composables/**/index.{ts,js,mjs,mts}',
