b00ecebf21137ad6761da3545c996732e13e4b85
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2025-07-27 12:30:54 +0900

    scss に変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['@/assets/css/main.css'],
+  css: ['@/assets/css/main.scss'],
   nitro: {
     preset: 'vercel',
     vercel: {
